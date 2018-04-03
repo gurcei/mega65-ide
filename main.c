f6a3b21eb9d9636b158677991f1c6194c411658c
gurcei <gurce.isikyildiz@gmail.com>
2018-04-03 18:15:58 +1000

    Added my first working example of the send_debug() function, used to send a string out the hypervisor serial port (to help assist in my debugging of this m65-ide app)

--- main.c
+++ main.c
@@ -5,6 +5,39 @@
 #include "windows.h"
 #include "input.h"
 
+unsigned char mybyte;
+
+#pragma optimize(off)
+void send_debug(unsigned char *s)
+{
+  int k;
+
+  // init M65 IO
+  POKE(0xd02f,0x47);
+  POKE(0xd02f,0x53);
+
+  for (k = 0; s[k] != '\0'; k++)
+  {
+    mybyte = s[k];
+    __asm__ ( "lda #$7C" );
+    __asm__ ( "ldy %v", mybyte); // (unsigned char)s[k]);
+    __asm__ ( "sta $D640" );
+    __asm__ ( "nop" );
+  }
+  // add a carriage return at the end
+    __asm__ ( "lda #$7C" );
+    __asm__ ( "ldy #$0a" );
+    __asm__ ( "sta $D640" );
+    __asm__ ( "nop" );
+
+  // reset it back to normal?
+  //POKE(0xd031,0);
+  // VIC-II IO mode
+  //POKE(0xd02f,0);
+}
+#pragma optimize(on)
+
+
 void main(void)
 {  
   // Set keyboard IRQ faster, so that cursor movement etc is faster
@@ -14,6 +47,8 @@ void main(void)
   setup_screen();
   initialise_windows();
   initialise_buffers();
+
+  send_debug("hello, how are you today?");
 
   while(!poll_keyboard());
 
